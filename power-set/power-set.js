6c8924af0ec024d915cef021238a73b6ad0d6691
Utsab Bera <nCoder24@users.noreply.github.com>
2023-05-14 23:12:54 +0530

    change approach to adding to powerSetOfRest

--- power-set/power-set.js
+++ power-set/power-set.js
@@ -1,17 +1,17 @@
-const result = [[]];
-
 const powerSet = (set) => {
-  if (set.length === 0) return;
+  if (set.length === 0) return [[]];
 
-  powerSet(set.slice(1));
-  result.push(...result.map((subSet) => [set[0], ...subSet]));
+  const powerSetOfRest = powerSet(set.slice(1));
+
+  return [
+    ...powerSetOfRest,
+    ...powerSetOfRest.map((subSet) => [set[0], ...subSet]),
+  ];
 };
 
 const main = () => {
-  const randomSet = new Array(15).fill().map((_, i) => i + 1);
-  powerSet(randomSet);
-
-  console.log(result.length);
-}
+  const randomSet = new Array(24).fill().map((_, i) => i + 1);
+  console.log(powerSet(randomSet));
+};
 
 main();
