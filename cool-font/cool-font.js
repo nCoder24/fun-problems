99d67fa4b4c7072b211dd997c8a4faa9ac28b74d
Utsab Bera <nCoder24@users.noreply.github.com>
2023-05-14 20:31:48 +0530

    cool-font takes input from cli args

--- cool-font/cool-font.js
+++ cool-font/cool-font.js
@@ -7,18 +7,33 @@ const applyFont = (font, text) =>
   render(text.split("").map((alphabet) => font[alphabet]));
 
 const parse = (fontDescription) => {
-  const alphabets = String.fromCharCode(...range("A".charCodeAt(0), "Z".charCodeAt(0)));
-  const identity = (...elements) => elements;
+  const alphabets = String.fromCharCode(
+    ...range("A".charCodeAt(0), "Z".charCodeAt(0))
+  );
   const [width, height, ...fontData] = fontDescription.split("\n");
-  const glyphs = map(identity, ...fontData.slice(0, height).map(row => chunk(row, width)));
+
+  const identity = (...elements) => elements;
+  const glyphs = map(
+    identity,
+    ...fontData.slice(0, height).map((row) => chunk(row, width))
+  );
 
   return Object.fromEntries(glyphs.map((glyph, i) => [alphabets[i], glyph]));
-}
+};
+
+const readFile = (file) => {
+  try {
+    return fs.readFileSync(file, "utf-8");
+  } catch {
+    console.log("Error reading file");
+  }
+};
 
 const main = () => {
-  const font = parse(fs.readFileSync("resources/font", "utf-8"));
+  const [fontFile, text] = process.argv.slice(2);
+  const font = parse(readFile(fontFile));
 
-  console.log(applyFont(font, "TEXT"));
-}
+  console.log(applyFont(font, text));
+};
 
 main();
