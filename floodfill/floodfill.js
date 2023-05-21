d88c58e0650ad7a536a3d1dc17027b5c54a432c0
Utsab Bera <nCoder24@users.noreply.github.com>
2023-05-21 15:02:11 +0530

    add floodfill problem

---
+++ floodfill/floodfill.js
@@ -0,0 +1,25 @@
+const makeBoundary = (points) => {
+  const lookup = Object.fromEntries(points.map(x => [x, true]));
+  return (point) => point in lookup;
+};
+
+const pointsInside = {};
+
+const floodfill = (start, isOnBoundary) => {
+  if (start in pointsInside || isOnBoundary(start)) return;
+  pointsInside[start] = start;
+
+  floodfill([start[0], start[1] + 1], isOnBoundary);
+  floodfill([start[0], start[1] - 1], isOnBoundary);
+  floodfill([start[0] + 1, start[1]], isOnBoundary);
+  floodfill([start[0] - 1, start[1]], isOnBoundary);
+};
+
+const main = () => {
+  const [start, ...isOnBoundary] = require("./resources/input1.json");
+  floodfill(start, makeBoundary(isOnBoundary));
+
+  console.log(Object.values(pointsInside));
+};
+
+main();
