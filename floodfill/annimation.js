32911e46479d588cb22d3f951c7c0dc61b010df6
Utsab Bera <nCoder24@users.noreply.github.com>
2023-05-22 12:33:28 +0530

    add annimation to inside points

--- floodfill/annimation.js
+++ floodfill/annimation.js
@@ -1,23 +1,44 @@
 const esc = "\033";
 const style = (text, code) => `${esc}[${code}m${text}${esc}[0m`;
-const printAt = (text, line, column) => console.log(
+const printAt = (text, line, column) => process.stdout.write(
   `${esc}[${line + 1};${column * 2 + 1}f${text}`
-);
+  );
 
+const makeCoursorInvisible = () => {
+  process.stdout.write(esc + "[?25l");
+}
+
+const makeCoursorVisible = () => {
+  process.stdout.write(esc + "[?25h");
+}
+
+const blackBox = style("  ", 40);
+const blueBox = style("  ", 44);
+const yellowBox = style("  ", 43);
+  
 const paintGrid = (row, col) => {
-  const yellowBox = style("  ", 43);
   console.clear();
   console.log(`${yellowBox.repeat(col)}\n`.repeat(row));
 };
 
 const colorPoints = (points) => {
-  const blackBox = style("  ", 40);
   points.forEach(([row, col]) => {
     printAt(blackBox, row, col);
   });
 };
 
+const printPointsInInterval = (points, interval) => {
+  if(points.length === 0) return;
+
+  setTimeout(() => {
+    const [row, col] = points[0];
+    printAt(blueBox, row, col);
+    printPointsInInterval(points.slice(1), interval);
+  }, interval);
+}
+
 exports.paintGrid = paintGrid;
 exports.colorPoints = colorPoints;
-exports.style = style;
-exports.printAt = printAt;
+exports.printPointsInInterval = printPointsInInterval;
+exports.makeCoursorInvisible = makeCoursorInvisible;
+exports.makeCoursorVisible = makeCoursorVisible;
--- floodfill/main.js
+++ floodfill/main.js
@@ -1,21 +1,19 @@
-const { paintGrid, colorPoints, printAt, style } = require("./annimation");
+const { paintGrid, colorPoints, printPointsInInterval, makeCoursorInvisible, makeCoursorVisible } = require("./annimation");
 const { calculateInsidePoints } = require("./floodfill");
 
 const main = () => {
-  const [insidePoint, ...barriers] = require("./resources/input1.json");
+  const [insidePoint, ...barriers] = require("./resources/input2.json");
   const pointsInside = calculateInsidePoints(insidePoint, barriers);
 
   const [maxRow, maxCol] = barriers.reduce(([maxRow, maxCol], [row, col]) => {
     return [Math.max(maxRow, row), Math.max(maxCol, col)];
   });
 
+  makeCoursorInvisible();
+
   paintGrid(maxRow + 1, maxCol + 1);
   colorPoints(barriers);
-
-  const blueBox = style("  ", 44);
-  pointsInside.forEach(([row, col]) => {
-      printAt(blueBox, row, col);
-  });
+  printPointsInInterval(pointsInside, 500);
 
   process.stdin.read();
 };
