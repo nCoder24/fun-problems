b6de4eaa053dc30017829ec3699e026d6cf2b2ce
Utsab Bera <nCoder24@users.noreply.github.com>
2023-05-22 10:44:35 +0530

    use in operator in boundary lookup

--- floodfill/floodfill.js
+++ floodfill/floodfill.js
@@ -1,5 +1,5 @@
 const floodfill = (pointsInside, start, boundary) => {
-  if (start in pointsInside || boundary[[start]]) return;
+  if (start in pointsInside || start in boundary) return;
   pointsInside[start] = start;
 
   const [x, y] = start;
@@ -11,7 +11,7 @@ const floodfill = (pointsInside, start, boundary) => {
 
 const calculateInsidePoints = (insidePoint, barriers) => {
   const pointsInside = {};
-  boundary = Object.fromEntries(barriers.map((point) => [point, true]));
+  boundary = Object.fromEntries(barriers.map((point) => [point]));
 
   floodfill(pointsInside, insidePoint, boundary);
   return Object.values(pointsInside);
